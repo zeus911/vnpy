40b3bb90150b4aa4f24a80f955dd61492b72f3ad
renxg <renxg@renxgdeMacBook-Pro.local>
2017-04-05 16:52:18 +0800

    none

--- vn.trader/vtServer.py
+++ vn.trader/vtServer.py
@@ -44,7 +44,9 @@ class VtServer(RpcServer):
         self.register(self.engine.getOrder)
         self.register(self.engine.getAllWorkingOrders)
         self.register(self.engine.getAllGatewayNames)
-        
+        self.register(self.engine.drEngine)
+        self.register(self.engine.rmEngine)
+
         # 注册事件引擎发送的事件处理监听
         self.engine.eventEngine.registerGeneralHandler(self.eventHandler)
         
