c0308c04fb0b2350441201548ba109de0fe99cdd
renxg <renxg@renxgdeMacBook-Pro.local>
2017-04-21 02:07:31 +0800

    add language

--- vn.trader/language/chinese/text.py
+++ vn.trader/language/chinese/text.py
@@ -131,3 +131,21 @@ DATABASE_CONNECTING_FAILED = u'MongoDB连接失败'
 DATA_INSERT_FAILED = u'数据插入失败，MongoDB没有连接'
 DATA_QUERY_FAILED = u'数据查询失败，MongoDB没有连接'
 DATA_UPDATE_FAILED = u'数据更新失败，MongoDB没有连接'
+
+
+LOADING_ERROR = u'读取连接配置出错，请检查'
+CONFIG_KEY_MISSING = u'连接配置缺少字段，请检查'
+
+DATA_SERVER_CONNECTED = u'行情服务器连接成功'
+DATA_SERVER_DISCONNECTED = u'行情服务器连接断开'
+DATA_SERVER_LOGIN = u'行情服务器登录完成'
+DATA_SERVER_LOGOUT = u'行情服务器登出完成'
+
+TRADING_SERVER_CONNECTED = u'交易服务器连接成功'
+TRADING_SERVER_DISCONNECTED = u'交易服务器连接断开'
+TRADING_SERVER_AUTHENTICATED = u'交易服务器验证成功' 
+TRADING_SERVER_LOGIN = u'交易服务器登录完成'
+TRADING_SERVER_LOGOUT = u'交易服务器登出完成'
+
+SETTLEMENT_INFO_CONFIRMED = u'结算信息确认完成'
+CONTRACT_DATA_RECEIVED = u'交易合约信息获取完成'
